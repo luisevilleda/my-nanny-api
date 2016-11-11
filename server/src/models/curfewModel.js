fc402aee3a13c0e3d55ea34a999a27e3e3df3c94
Luis Villeda <luisevilleda@gmail.com>
2016-11-11 15:11:45 -0800

    Edit model to only have 1 UTC time

--- server/src/models/curfewModel.js
+++ server/src/models/curfewModel.js
@@ -2,8 +2,7 @@ import Sequelize from 'sequelize';
 import db from '../connection';
 
 const Curfew = db.define('curfew', {
-  startUTC: Sequelize.INTEGER,
-  endUTC: Sequelize.INTEGER,
+  utc: Sequelize.INTEGER,
 });
 
 export default Curfew;
