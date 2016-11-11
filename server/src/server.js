5881d07fd7db6f62d945152108990eb7ddffe38f
Luis Villeda <luisevilleda@gmail.com>
2016-11-11 15:20:17 -0800

    Fix static file directory location

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'docs/public')));
+app.use(express.static(path.join(__dirname, 'docs/')));
 
 routes(app);
 
