5b79e45df021bd8df978029afe30e03398377f34
Luis Villeda <luisevilleda@gmail.com>
2016-11-11 15:09:45 -0800

    Document child repo

--- server/src/repositories/childRepository.js
+++ server/src/repositories/childRepository.js
@@ -1,10 +1,23 @@
 import Child from '../models/childModel';
 
+/** @namespace childRepository */
 const childRepository = {
+  /**
+    * @function create
+    * @memberof childRepository
+    * @param {object} data
+    * @param data.name - Child's first name
+    * @param data.phone - Child's phone number
+  */
   create: function createChild({ name, phone }) {
     return Child.build({ name, phone });
   },
-
+  /**
+    * @function update
+    * @memberof childRepository
+    * @param {object} child - Instance of a child from the db.
+    * @param {object} changedAttributes - Obj with the keys and attributes to be updated.
+  */
   update: (child, changedAttributes) => {
     child.update(changedAttributes)
     .then((res) => {
@@ -12,7 +25,11 @@ const childRepository = {
       return res;
     });
   },
-
+  /**
+    * @function destroy
+    * @memberof childRepository
+    * @param {object} child - Instance of a child from the db.
+  */
   destroy: child => child.destroy(),
 
   save: child => child.save(),
